ff27090d7a6d12dfeebb6f75300c5d074d38b4a2
Allen <allenwang28@yahoo.com>
2018-12-02 07:22:37 +0000

    Fixed kinetics audio extraction

--- utils/video_audio_kinetics.py
+++ utils/video_audio_kinetics.py
@@ -16,23 +16,10 @@ def class_process(dir_path, dst_dir_path, class_name):
     if '.mp4' not in file_name:
       continue
     name, ext = os.path.splitext(file_name)
-    dst_directory_path = os.path.join(dst_class_path, name)
+    dst_path = os.path.join(dst_class_path, name + '.wav')
 
     video_file_path = os.path.join(class_path, file_name)
-    try:
-      if os.path.exists(dst_directory_path):
-        if not os.path.exists(os.path.join(dst_directory_path, 'image_00001.jpg')):
-          subprocess.call('rm -r \"{}\"'.format(dst_directory_path), shell=True)
-          print('remove {}'.format(dst_directory_path))
-          os.mkdir(dst_directory_path)
-        else:
-          continue
-      else:
-        os.mkdir(dst_directory_path)
-    except:
-      print(dst_directory_path)
-      continue
-    cmd = 'ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}'.format(video_file_path, dst_path)
+    cmd = 'ffmpeg -i \"{}\" -ab 160k -ac 2 -ar 44100 -vn \"{}\"'.format(video_file_path, dst_path)
     print(cmd)
     subprocess.call(cmd, shell=True)
     print('\n')
